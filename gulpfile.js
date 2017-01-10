110808b8bd64230635ce39d7888f9549475780f5
thomas ricci <grisou13@hotmail.fr>
2017-01-10 07:39:08 +0100

    added menu

--- gulpfile.js
+++ gulpfile.js
@@ -128,7 +128,7 @@ gulp.task("copy-images",function(){
   .pipe(gulp.dest("./dist/images"));
 })
 gulp.task("watch-images",function() {
-  return gulp.watch("./src/images/*.+(svg|png|jpeg|jpg)",["copy-images"])
+  return gulp.watch("./src/images/**/*.+(svg|png|jpeg|jpg)",["copy-images"])
 })
 gulp.task('default', ['copy-fonts',"copy-templates","copy-images", 'sass', "watch-templates", "watch-images", 'watch-sass', 'watchify', 'browsersync']);
 gulp.task('build', ['copy-fonts',"copy-templates","copy-images", 'sass', 'browserify']);
@@ -140,103 +140,3 @@ gulp.task("deploy",["build"],function(cb){
     cb(err);
   });
 })
-//console.log(sassConfig)
-//js
-// function compile(watch) {
-//   var bundler = watchify(browserify('./src/index.js', { debug: true }).transform(babel));
-//
-//   function rebundle() {
-//     bundler.bundle()
-//       .on('error', function(err) { console.error(err); this.emit('end'); })
-//       .pipe(source('build.js'))
-//       .pipe(buffer())
-//       .pipe(sourcemaps.init({ loadMaps: true }))
-//       .pipe(sourcemaps.write('./'))
-//       .pipe(gulp.dest('./src/js'));
-//   }
-//   functions sassiffy(){
-//
-//   }
-//   if (watch) {
-//     bundler.on('update', function() {
-//       console.log('-> bundling...');
-//       rebundle();
-//     });
-//   }
-//
-//   rebundle();
-// }
-//
-// function watch() {
-//   return compile(true);
-// };
-// gulp.task('scripts',function(){
-//   return gulp.src([paths.jquery+"dist/jquery.min.js",paths.bootstrap + "/javascripts/bootstrap.min.js",paths.scripts])
-//         .pipe(sourcemaps.init())
-//         .pipe(browserify({
-//           insertGlobals : true,
-//           debug : true
-//         }))
-//         .pipe(babel({
-//             presets: ['es2015','react']
-//         }))
-//
-//         .pipe(concat('app.js'))
-//         .pipe(sourcemaps.write('.'))
-//         .pipe(gulp.dest("./src/js"))
-//         .pipe(reload({match: '**/*.js',stream:true}));
-// });
-//
-// //styles
-// //icons
-// gulp.task('fonts', function() { 
-//     return gulp.src([paths.fontawesome + "fonts/**.*", paths.bootstrap + "fonts/bootstrap/**.*"]) 
-//         .pipe(gulp.dest('./dist/fonts')); 
-// });
-// gulp.task("images", function(){
-//   return gulp.src([paths.images]).pipe(gulp.dest("./dist/images"))
-// })
-// gulp.task('styles',["fonts"], function() {
-//     return gulp.src(paths.styles)
-//           .pipe(sourcemaps.init())
-//           .pipe(sass(sassConfig))
-//           .on('error', sass.logError)
-//           .pipe(sourcemaps.write())
-//           //.pipe(concat('app.css'))
-//           .pipe(autoprefix('last 2 version'))
-//           .pipe(gulp.dest('./src/app.css'))
-//
-//           .pipe(reload({match: '**/*.css',stream:true}));
-// });
-// gulp.task("build",function(){
-//   let sassStream = gulp.src(paths.styles).pipe(sass(sassConfig));
-//   let scriptStream = gulp.src([paths.jquery+"dist/jquery.min.js",paths.bootstrap + "/javascripts/bootstrap.min.js",paths.scripts])
-//
-//         .pipe(babel({
-//             presets: ['es2015','react']
-//         }))
-//         .pipe(browserify({
-//           insertGlobals : true,
-//           debug : false
-//         }))
-//
-//   return gulp.src(paths.html)
-//         .pipe(useref({additionalStream : [sassStream,scriptStream]},lazypipe().pipe(sourcemaps.init, { loadMaps: true }) ))
-//         .pipe(sourcemaps.write('maps'))
-//         .pipe(gulp.dest("./dist"));
-// });
-//
-// // Static Server + watching scss/html files
-// gulp.task('serve', ['styles','scripts'], function() {
-//
-//     browserSync.init({
-//         server: "./src"
-//     });
-//     gulp.watch("./src/*.html").on('change', browserSync.reload);
-//     //gulp.watch([paths.styles,paths.scripts,paths.html,paths.images]).on('change', browserSync.reload);;
-// });
-// gulp.task("default",["styles","scripts"],function(){});
-// //deafult task
-// // gulp.task('default',['styles','fonts','scripts','html','images'],function() {
-// //
-// // });
