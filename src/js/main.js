20217b89147602413097a9b32add0362c52dc19a
Grisou13 <grisou13@hotmail.fr>
2017-01-15 22:27:02 +0100

    Still need to fix menu position (z-index) in page.html. \nNeed to get real images \n create svg's also

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,27 @@
 //require("./modernizr");
 require("./polyfills");
-var jQuery = window.jQuery = window.$ = require('jquery')
+var $ = jQuery = window.jQuery = window.$ = require('jquery')
 // Bootstrap doesn't have a "main" field / export anything =(
 //var bootstrap = require('bootstrap-sass/assets/javascripts/bootstrap')
 var Materialize = require("materialize")
-global.particleJS = window.particleJS = require("particle")
+
 jQuery.noConflict(true);
 var NavBar = require('./nav');
-var Hero = require('./hero');
-//
 let nav = new NavBar();
-let c = new Hero();
+if(location.pathname == "/")
+{
+  //global.particleJS = window.particleJS = require("particle")
+  var Hero = require('./hero');
+  let c = new Hero();
+
+
+}
+
+// document.querySelectorAll("img").forEach(function(el){
+//   if(!el.classList.contains("logo"))
+//     el.src = "http://lorempixel.com/640/480/city"
+// });
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
+//document.querySelectorAll(".parallax").forEach((el)=>el.parallax())
